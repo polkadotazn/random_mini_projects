f721b0c13b21875375f87c2ad126ad9823dbf2e7
Louisa Xue <xuelex@gmail.com>
2018-06-22 10:16:20 -0700

    Practice react

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/:id', function(req, res){
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/first_template', function(req, res) {
+  res.render('first_view');
+});
+
+app.get('/:id([0-9]*)', function(req, res){
   res.send('The id you specified is ' + req.params.id);
 });
 
-app.get('things/:name/:id', function(req, res){
+app.get('/things/:name/:id', function(req, res){
   res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
 });
+//
+// app.use(function(req, res, next){
+//   console.log("New request at " + Date.now());
+// });
 
-app.get('*', function(req,res){
-  res.send('Sorry, this is an invalid URL.');
+app.use('/things', function(req, res, next){
+  console.log("Request for things received at " + Date.now());
+  next();
 });
+//
+// app.get('/things', function(req, res){
+//   res.send('Things');
+// });
 
 var things = require('./things.js');
 
 app.use('/things', things);
+
+app.get('*', function(req,res){
+  res.send('Sorry, this is an invalid URL.');
+});
 
 app.listen(3000);
---
+++ javascript/calculator.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  
+}
